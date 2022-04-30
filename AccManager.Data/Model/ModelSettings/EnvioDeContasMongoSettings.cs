4d5fa62c0e71a09fa1271278d39aacf1acc32c7f
Luiz Davi <davi-sdb@hotmail.com>
2022-04-30 18:18:48 -0300

    initial commit

---
+++ AccManager.Data/Model/ModelSettings/EnvioDeContasMongoSettings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AccManager.Data.Models.ModelSettings
+{
+    public class EnvioDeContasMongoSettings : IEnvioDeContasMongoSettings
+    {
+        public string DataBaseName { get; set; }
+        public string ConnectionString { get; set; }
+        public string SizPag { get; set; }
+    }
+
+    public interface IEnvioDeContasMongoSettings
+    {
+        public string DataBaseName { get; set; }
+        public string ConnectionString { get; set; }
+        public string SizPag { get; set; }
+    }
+}
